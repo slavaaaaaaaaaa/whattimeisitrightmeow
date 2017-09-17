8a37634191aafb86ecbec96aee1cc698243abbb7
Slava Maslennikov @ slon <slava@allweretaken.xyz>
2017-09-17 11:07:23 -0700

    Make this thing worse

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -e
+
 while true; do
     date +"%H %M" > time.txt
     cat time.txt
-    git add . && git commit -m "Can't you see I'm updating the time?" && git push origin master
+    git add time.txt
+    git commit -m "Can't you see I'm updating the time?"
+    git pull upstream master --rebase
+    git push origin master
     sleep 60
 done

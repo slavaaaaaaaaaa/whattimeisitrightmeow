5b604df5ad2bc48d0e4a2548bd09f74e7537e0fd
Slava Maslennikov @ slon <slava@allweretaken.xyz>
2017-09-17 11:23:17 -0700

    Some gits are picky?

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@ while true; do
     sed -i 's&<p can i put a marker here?.*$&<p can i put a marker here?>'"$(date +"%H %M")"'</p>&' index.html
     git add index.html
     git commit -m "Can't you see I'm updating the time?"
-    git pull origin master --rebase
+    git pull --rebase origin master
     git push origin master
     sleep 60
 done

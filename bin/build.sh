37b8bbbbb0772a4887e9333cdc81421d1f83dac6
Slava Maslennikov @ slon <slava@allweretaken.xyz>
2017-09-17 11:13:03 -0700

    Fix pulling from upstream because habits

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@ while true; do
     sed -i 's&<p can i put a marker here?.*$&<p can i put a marker here?>'"$(date +"%H %M")"'</p>&' index.html
     git add index.html
     git commit -m "Can't you see I'm updating the time?"
-    git pull upstream master --rebase
+    git pull origin master --rebase
     git push origin master
     sleep 60
 done

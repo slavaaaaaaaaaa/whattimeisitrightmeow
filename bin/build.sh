c275eab9102c0ea3746a43facae9720a264ab5e6
Henrik Hussfelt <henrik@henrik.se>
2017-09-18 15:05:13 +0200

    #3 add plain-text version of time

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,8 @@ set -e
 while true; do
     sed -i 's&<p can i put a marker here?.*$&<p can i put a marker here?>'"$(date +"%H %M")"'</p>&' index.html
     git add index.html
+    echo $(date +"%H %M") > time.txt
+    git add time.txt
     git commit -m "Can't you see I'm updating the time?"
     git pull --rebase origin master
     git push origin master

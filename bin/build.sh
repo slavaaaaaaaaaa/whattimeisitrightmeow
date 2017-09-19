9dbc8ff26718f0e9d20d4af5a2a62eb3643199b1
Michael Langfermann <versable@gmail.com>
2017-09-19 14:35:18 +0200

    Store date as var, use fork and file lock mechanism
    git pull/push (master) is bound to the network, and the script will hang
    if the network times out. This will keep committing despite of the
    results of git push/pull

--- bin/build.sh
+++ bin/build.sh
@@ -6,13 +6,22 @@ git config user.name time
 git config user.email time@allweretaken.xyz
 git config commit.gpgsign false
 
+if [ -e lock ]; then
+    rm lock
+fi
+
 while true; do
-    sed -i 's&<p can i put a marker here?.*$&<p can i put a marker here?>'"$(date +"%H %M")"'</p>&' index.html
+    date=$(date +"%H %M")
+    sed -i 's&<p can i put a marker here?.*$&<p can i put a marker here?>'$date'</p>&' index.html
+    echo $date > time.txt
     git add index.html
-    echo $(date +"%H %M") > time.txt
     git add time.txt
     git commit -m "Can't you see I'm updating the time?"
-    git pull --rebase origin master
-    git push origin master
+    if ! [ -e lock ]; then
+        (touch lock; \
+         git pull --rebase origin master: \
+         git push origin master; \
+         rm lock)&
+    fi
     sleep $((60 - $(date +%-S)))
 done
